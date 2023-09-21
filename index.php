9105e1033a1fdaea9d1e2b87ce9942ece022aaca
unknown <danuar.anardha007@gmail.com>
2023-09-22 00:56:49 +0700

    change form method to POST

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+ <?php 
 session_start();
 include('config/config.php');
 if (isset($_POST['login'])) {
@@ -11,7 +11,7 @@ if (isset($_POST['login'])) {
     $stmt->bind_result($login_permission, $login_username, $login_pass, $login_id); //bind result 
     $rs = $stmt->fetch();
     $_SESSION['login_id'] = $login_id; 
-    if ($login_username == "admin" && $login_pass == "admin") { //if its sucessfull 
+    if ($rs && $login_permission == '1') { //if its sucessfull 
       // print("Login berhasil!");
       header("location:pages_admin_dashboard.php"); 
     } else { 
@@ -59,14 +59,6 @@ require_once('partials/_head.php');
 
     <!-- Template Main CSS File -->
     <link href="assets/css/style.css" rel="stylesheet" />
-
-    <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Updated: Sep 18 2023 with Bootstrap v5.3.2
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
   </head>
 
   <body>
@@ -102,7 +94,7 @@ require_once('partials/_head.php');
                       </p>
                     </div>
 
-                    <form class="row g-3 needs-validation" novalidate>
+                    <form class="row g-3 needs-validation" method="POST" novalidate>
                       <div class="col-12">
                         <label for="yourUsername" class="form-label"
                           >Username</label
@@ -171,10 +163,6 @@ require_once('partials/_head.php');
                 </div>
 
                 <div class="credits">
-                  <!-- All the links in the footer should remain intact. -->
-                  <!-- You can delete the links only if you purchased the pro version. -->
-                  <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                  <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
                   Designed by
                   <a href="https://bootstrapmade.com/">BootstrapMade</a>
                 </div>
